ebed18f30e3123b654cda47e071888f9d4db8ed8
Xiaoai Zhu <xzhu411@gatech.edu>
2025-05-16 04:33:30 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#pragma message("Glad included successfully")
+#pragma message("GLFW included successfully")
+
+int main() {
+    if (!glfwInit()) return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Bunny + Water", NULL, NULL);
+    if (!window) { glfwTerminate(); return -1; }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
